9a048486ebd15344f792536abbff88c0e003e00c
luishighnest <luishighnest@users.noreply.github.com>
2026-08-20 15:33:40 +0200

    v1.10.47: setContent videos per le categorie Sky - Estuary usa il layout a caselle completo: nome+data sulla casella e guida+scadenza a colori sopra l'anteprima

--- plugin.video.kodiakso/default.py
+++ plugin.video.kodiakso/default.py
@@ -363,22 +363,18 @@ def _exp_col(exp):
     return '00FF00'
 
 
-def _sky_tile_prog(cur):
-    name = ' '.join(str(cur[2]).split()[:3])
-    if len(name) > 12:
-        name = name[:12].rstrip() + '\u2026'
-    return '%02d:%02d %s' % (cur[0].hour, cur[0].minute, name)
+def _sky_epg_full(cur):
+    return '%02d:%02d %s' % (cur[0].hour, cur[0].minute, _epg_short(cur[2], 30))
 
 
 def _sky_parts(title, exp, prog=''):
     label = '[COLOR snow]%s[/COLOR]' % title
     l2 = ''
     if prog:
-        label += ' [COLOR 888888]\u2022[/COLOR] [COLOR 00FF00]%s[/COLOR]' % prog
         l2 = '[COLOR 00FF00]%s[/COLOR]' % prog
     if exp:
-        t = exp.strftime('%d/%m/%Y %H:%M')
         ts = exp.strftime('%d/%m %H:%M')
+        t = exp.strftime('%d/%m/%Y %H:%M')
         col = _exp_col(exp)
         label += ' [COLOR 888888]\u2022[/COLOR] [COLOR %s]%s[/COLOR]' % (col, ts)
         l2 = ((l2 + '    ') if l2 else '') + '[COLOR %s]SCADENZA %s[/COLOR]' % (col, t)
@@ -863,7 +859,7 @@ def sky_cat_view(cat, back=''):
                 cur, nxt = _epg_now(cid, epg)
                 prog = ''
                 if cur:
-                    prog = _sky_tile_prog(cur)
+                    prog = _sky_epg_full(cur)
                 label, l2, tname = _sky_parts(title, exp, prog)
                 li = xbmcgui.ListItem(label=label)
                 if l2:
@@ -895,6 +891,7 @@ def sky_cat_view(cat, back=''):
             log('sky_cat TB: ' + traceback.format_exc())
         except Exception:
             pass
+    xbmcplugin.setContent(HANDLE, 'videos')
     xbmcplugin.endOfDirectory(HANDLE)
 
 
