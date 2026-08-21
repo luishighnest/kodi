db0f2705b04a7e12c5711be4d5b838a33c916f3c
luishighnest <luishighnest@users.noreply.github.com>
2026-08-21 22:15:12 +0200

    v1.10.69: OSD title update

--- plugin.video.kodiakso/default.py
+++ plugin.video.kodiakso/default.py
@@ -429,7 +429,7 @@ def _sky_expiry(cid):
     return exp
 
 
-def resolve_sky(parIn, title):
+def resolve_sky(parIn, title, prog=''):
     try:
         resp = requests.get(API + '?numTest=A1A159&id=' + urllib.parse.quote(parIn),
                             headers={'User-Agent': API_UA}, timeout=20)
@@ -470,6 +470,8 @@ def resolve_sky(parIn, title):
     pname = title
     if parIn.startswith('skysport') and not pname.upper().startswith('SKY'):
         pname = 'SKY ' + pname
+    if prog:
+        pname += ' • ' + prog
     li.setLabel(pname)
     li.setInfo('video', {'title': pname})
     return li
@@ -889,7 +891,7 @@ def sky_cat_view(cat, back=''):
                 if nxt:
                     lines.append('%02d:%02d %s' % (nxt[0].hour, nxt[0].minute, _epg_short(nxt[2], 60)))
                 li.setInfo('video', {'title': tname, 'plot': ' | '.join(lines)})
-                url = BASE + '?action=skyplay&id=' + urllib.parse.quote(cid) + '&t=' + urllib.parse.quote(title)
+                url = BASE + '?action=skyplay&id=' + urllib.parse.quote(cid) + '&t=' + urllib.parse.quote(title) + '&p=' + urllib.parse.quote(prog)
                 xbmcplugin.addDirectoryItem(HANDLE, url, li, isFolder=False)
             except Exception as e:
                 log('sky_cat item %s: %s' % (cid, e))
@@ -2524,7 +2526,7 @@ def main():
         elif action == 'skycat':
             sky_cat_view(query.get('cat', [''])[0], query.get('back', [''])[0])
         elif action == 'skyplay':
-            li = resolve_sky(query.get('id', [''])[0], query.get('t', [''])[0])
+            li = resolve_sky(query.get('id', [''])[0], query.get('t', [''])[0], query.get('p', [''])[0])
             xbmcplugin.setResolvedUrl(HANDLE, True, li)
         elif action == 'sportzx':
             sportzx_view()
