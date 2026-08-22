cdf3125fd3731a9e42aadd3de3ece2d321726bae
luishighnest <luishighnest@users.noreply.github.com>
2026-08-22 13:17:09 +0200

    v1.10.77: Rimossa duplicazione prefisso Kodi nei titoli player delle serie TV

--- plugin.video.kodiakso/default.py
+++ plugin.video.kodiakso/default.py
@@ -1086,7 +1086,7 @@ def resolve_v2(id_, mtype='movie', season='0', episode='0', title='', eptitle=''
 
     li = xbmcgui.ListItem(label=lbl(player_title))
     li.setPath(purl)
-    li.setInfo('video', {'title': player_title, 'mediatype': 'movie' if mtype == 'movie' else 'episode'})
+    li.setInfo('video', {'title': player_title, 'tvshowtitle': '', 'season': 0, 'episode': 0, 'mediatype': 'movie' if mtype == 'movie' else 'video'})
     if ptype == 'hls' or purl.endswith('.m3u8'):
         headers = urllib.parse.urlencode({'User-Agent': VIXSRC_UA, 'Referer': embed, 'Origin': base})
         li.setProperty('inputstream', 'inputstream.adaptive')
@@ -1436,7 +1436,7 @@ def resolve_scws(parIn, title, eptitle='', season=0, episode=0):
         player_title = title or ''
 
     li = xbmcgui.ListItem(path=urlSc, label=lbl(player_title), offscreen=True)
-    li.setInfo('video', {'title': player_title})
+    li.setInfo('video', {'title': player_title, 'tvshowtitle': '', 'season': 0, 'episode': 0})
     li.setContentLookup(False)
     li.setMimeType('application/x-mpegURL')
     li.setProperty('inputstream', 'inputstream.adaptive')
