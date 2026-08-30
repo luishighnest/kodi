9ede22095943e399694bf9c4207bc6925af018b2
luishighnest <luishighnest@users.noreply.github.com>
2026-08-30 16:01:40 +0200

    v1.11.55: Aggiunta dipendenza formale script.module.pycryptodome in addon.xml e decifratore AES-GCM perfetto e verificato per sky2.json e test.json

--- plugin.video.kodiakso/default.py
+++ plugin.video.kodiakso/default.py
@@ -24,181 +24,37 @@ import xbmc
 import requests
 
 
-# === ZADONKAIS / KODIAKSO UNIVERSAL ZERO-DEPENDENCY AES-256-GCM DECRYPT HELPER ===
+# === ZADONKAIS / KODIAKSO AES-256-GCM DECRYPT HELPER ===
 ZADONKAIS_MASTER_PW = "2941"
 ZADONKAIS_SALT = b"zadonkais_secure_salt_2026"
 
-S_BOX = (
-    0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
-    0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
-    0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
-    0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
-    0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
-    0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
-    0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
-    0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
-    0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
-    0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
-    0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5E, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
-    0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
-    0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
-    0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
-    0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
-    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
-)
-RCON = (0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36)
-
-def _sub_word(w):
-    return ((S_BOX[(w >> 24) & 0xFF] << 24) |
-            (S_BOX[(w >> 16) & 0xFF] << 16) |
-            (S_BOX[(w >> 8) & 0xFF] << 8) |
-            S_BOX[w & 0xFF])
-
-def _rot_word(w):
-    return ((w << 8) & 0xFFFFFFFF) | (w >> 24)
-
-def _aes_key_expansion_256(key_bytes):
-    import struct
-    w = list(struct.unpack('>8I', key_bytes))
-    for i in range(8, 60):
-        temp = w[i - 1]
-        if i % 8 == 0:
-            temp = _sub_word(_rot_word(temp)) ^ (RCON[(i // 8) - 1] << 24)
-        elif i % 8 == 4:
-            temp = _sub_word(temp)
-        w.append(w[i - 8] ^ temp)
-    return w
-
-def _xtime(a):
-    return (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
-
-def _aes_encrypt_block(block_bytes, round_keys):
-    s = [list(block_bytes[i*4:(i+1)*4]) for i in range(4)]
-    for col in range(4):
-        rk = round_keys[col]
-        s[col][0] ^= (rk >> 24) & 0xFF
-        s[col][1] ^= (rk >> 16) & 0xFF
-        s[col][2] ^= (rk >> 8) & 0xFF
-        s[col][3] ^= rk & 0xFF
-
-    for r in range(1, 14):
-        s0 = S_BOX[s[0][0]]
-        s1 = S_BOX[s[1][1]]
-        s2 = S_BOX[s[2][2]]
-        s3 = S_BOX[s[3][3]]
-        s4 = S_BOX[s[1][0]]
-        s5 = S_BOX[s[2][1]]
-        s6 = S_BOX[s[3][2]]
-        s7 = S_BOX[s[0][3]]
-        s8 = S_BOX[s[2][0]]
-        s9 = S_BOX[s[3][1]]
-        sa = S_BOX[s[0][2]]
-        sb = S_BOX[s[1][3]]
-        sc = S_BOX[s[3][0]]
-        sd = S_BOX[s[0][1]]
-        se = S_BOX[s[1][2]]
-        sf = S_BOX[s[2][3]]
-
-        s[0] = [_xtime(s0) ^ _xtime(s1) ^ s1 ^ s2 ^ s3,
-                s0 ^ _xtime(s1) ^ _xtime(s2) ^ s2 ^ s3,
-                s0 ^ s1 ^ _xtime(s2) ^ _xtime(s3) ^ s3,
-                _xtime(s0) ^ s0 ^ s1 ^ s2 ^ _xtime(s3)]
-        s[1] = [_xtime(s4) ^ _xtime(s5) ^ s5 ^ s6 ^ s7,
-                s4 ^ _xtime(s5) ^ _xtime(s6) ^ s6 ^ s7,
-                s4 ^ s5 ^ _xtime(s6) ^ _xtime(s7) ^ s7,
-                _xtime(s4) ^ s4 ^ s5 ^ s6 ^ _xtime(s7)]
-        s[2] = [_xtime(s8) ^ _xtime(s9) ^ s9 ^ sa ^ sb,
-                s8 ^ _xtime(s9) ^ _xtime(sa) ^ sa ^ sb,
-                s8 ^ s9 ^ _xtime(sa) ^ _xtime(sb) ^ sb,
-                _xtime(s8) ^ s8 ^ s9 ^ sa ^ _xtime(sb)]
-        s[3] = [_xtime(sc) ^ _xtime(sd) ^ sd ^ se ^ sf,
-                sc ^ _xtime(sd) ^ _xtime(se) ^ se ^ sf,
-                sc ^ sd ^ _xtime(se) ^ _xtime(sf) ^ sf,
-                _xtime(sc) ^ sc ^ sd ^ se ^ _xtime(sf)]
-
-        rk_idx = r * 4
-        for col in range(4):
-            rk = round_keys[rk_idx + col]
-            s[col][0] ^= (rk >> 24) & 0xFF
-            s[col][1] ^= (rk >> 16) & 0xFF
-            s[col][2] ^= (rk >> 8) & 0xFF
-            s[col][3] ^= rk & 0xFF
-
-    s0 = S_BOX[s[0][0]]
-    s1 = S_BOX[s[1][1]]
-    s2 = S_BOX[s[2][2]]
-    s3 = S_BOX[s[3][3]]
-    s4 = S_BOX[s[1][0]]
-    s5 = S_BOX[s[2][1]]
-    s6 = S_BOX[s[3][2]]
-    s7 = S_BOX[s[0][3]]
-    s8 = S_BOX[s[2][0]]
-    s9 = S_BOX[s[3][1]]
-    sa = S_BOX[s[0][2]]
-    sb = S_BOX[s[1][3]]
-    sc = S_BOX[s[3][0]]
-    sd = S_BOX[s[0][1]]
-    se = S_BOX[s[1][2]]
-    sf = S_BOX[s[2][3]]
-
-    out = bytearray(16)
-    out[0] = s0 ^ ((round_keys[56] >> 24) & 0xFF)
-    out[1] = s1 ^ ((round_keys[56] >> 16) & 0xFF)
-    out[2] = s2 ^ ((round_keys[56] >> 8) & 0xFF)
-    out[3] = s3 ^ (round_keys[56] & 0xFF)
-    out[4] = s4 ^ ((round_keys[57] >> 24) & 0xFF)
-    out[5] = s5 ^ ((round_keys[57] >> 16) & 0xFF)
-    out[6] = s6 ^ ((round_keys[57] >> 8) & 0xFF)
-    out[7] = s7 ^ (round_keys[57] & 0xFF)
-    out[8] = s8 ^ ((round_keys[58] >> 24) & 0xFF)
-    out[9] = s9 ^ ((round_keys[58] >> 16) & 0xFF)
-    out[10] = sa ^ ((round_keys[58] >> 8) & 0xFF)
-    out[11] = sb ^ (round_keys[58] & 0xFF)
-    out[12] = sc ^ ((round_keys[59] >> 24) & 0xFF)
-    out[13] = sd ^ ((round_keys[59] >> 16) & 0xFF)
-    out[14] = se ^ ((round_keys[59] >> 8) & 0xFF)
-    out[15] = sf ^ (round_keys[59] & 0xFF)
-    return bytes(out)
-
-def _aes_gcm_decrypt_pure(key_bytes, iv, ciphertext):
-    import struct
-    round_keys = _aes_key_expansion_256(key_bytes)
-    plaintext = bytearray(len(ciphertext))
-    counter = 2
-    for offset in range(0, len(ciphertext), 16):
-        cb = iv + struct.pack('>I', counter)
-        keystream = _aes_encrypt_block(cb, round_keys)
-        chunk_len = min(16, len(ciphertext) - offset)
-        for i in range(chunk_len):
-            plaintext[offset + i] = ciphertext[offset + i] ^ keystream[i]
-        counter += 1
-    return bytes(plaintext)
-
 def _zadonkais_decrypt(enc_b64, pw=ZADONKAIS_MASTER_PW):
+    if not enc_b64:
+        return {}
     try:
         key = hashlib.pbkdf2_hmac('sha256', pw.encode('utf-8'), ZADONKAIS_SALT, 100000, 32)
         raw = base64.b64decode(enc_b64)
         iv = raw[:12]
         ciphertext = raw[12:-16]
         
-        # 1. PyCryptodome (se presente in Kodi)
+        # 1. PyCryptodome (incluso in Kodi tramite script.module.pycryptodome)
         try:
             from Crypto.Cipher import AES
             cipher = AES.new(key, AES.MODE_GCM, nonce=iv)
-            return json.loads(cipher.decrypt(ciphertext).decode('utf-8'))
-        except Exception:
-            pass
+            pt = cipher.decrypt(ciphertext)
+            return json.loads(pt.decode('utf-8'))
+        except Exception as e1:
+            log('PyCryptodome decrypt ERR: %s' % e1)
 
-        # 2. Cryptography (se presente)
+        # 2. Cryptography
         try:
             from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-            return json.loads(AESGCM(key).decrypt(iv, raw[12:], None).decode('utf-8'))
-        except Exception:
-            pass
+            pt = AESGCM(key).decrypt(iv, raw[12:], None)
+            return json.loads(pt.decode('utf-8'))
+        except Exception as e2:
+            log('Cryptography decrypt ERR: %s' % e2)
 
-        # 3. Pure Python Nativo (zero dipendenze, 100% funzionante su Android, Firestick, Windows, Mac, Linux)
-        pt = _aes_gcm_decrypt_pure(key, iv, ciphertext)
-        return json.loads(pt.decode('utf-8'))
+        return {}
     except Exception as e:
         log('_zadonkais_decrypt ERR: %s' % e)
         return {}
